37c96488b9082be0cc9503c3cdbdd08f85213a7c
Mouth <adrien.charneau@edu.itescia.fr>
2020-01-09 09:29:53 +0100

    Initial Commit

---
+++ renduIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  renduIOS
+//
+//  Created by mc26 on 09/01/2020.
+//  Copyright © 2020 mc26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
